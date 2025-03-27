bbbd207d9202462199bd98299aabbcd67ee06453
Sawudamoha1 <sawuda.mohamed25@compscihigh.org>
2025-03-27 16:30:42 +0000

    Deliverable 3 Completed

--- index.js
+++ index.js
@@ -3,36 +3,37 @@
 // The variable name should have no spaces, begin with a lowercase letter, and describe the contents of a variable
 // e.g. For example, an animal variable would look like: let animal = "cat"
 
-
+let name= "Susu"
 
 // Use console.log() to print the variable by placing the variable name inside the parentheses.  
 // To run your code, in the Terminal, type node index.js and you should see your name show up!
 
-
+console.log(name)
 // Task 2: Reassign the variable to include your last name.
 // Reference the variable name (no let since you aren't making a new variable) and an equal sign to re-assign it
 // After the equals sign, put the variable name with a + sign to add your last name in quotes (with a space)
 
-
+name += " mohamed "
 
 // Use console.log to print the updated variable and run it in the Terminal to test
-
+console.log(name)
 
 // Task 3: Declare a variable 'greeting' and assign it a string that says "Hello, my name is " followed by the 'name' variable.  
 
-
+let greeting = "Hello my name is" + name
 
 // Print the variable and run it to test
-
+console.log(greeting)
 
 // Task 4: Declare a variable 'age' and assign it your age as a number.  
 // It's declared the same way as your name but with no quotes since age is a number.  
 
+let age = 17
 
 
 // Print "My age is" with your age and run it to test
 
-
+console.log("my age is" + age)
 // Task 5: Create an object called 'favoriteMovie' with keys: name (string), genre (string), rating (number).  
 // Use curly braces {} to define the object, and inside, write key-value pairs separated by colons (:). 
 // Each line has one key-value pair separated by a comma.  
@@ -42,30 +43,34 @@
 //     age: 27
 // }
 
-
+let favoriteMovie= {
+    name:"greys anatomy",
+    genre:"drama",
+    rating:9.5
+}
 
 // Print the object (same as any other variable) and run it to test
+console.log(favoriteMovie)
 
-
-// Task 10: Change the rating to be 5
+//  task 10: Change the rating to be 5
 // Access the 'rating' key using objectName.keyName
 // For example, changing Ms. Pham's age would look like: teacher.age = 28
 
-
+favoriteMovie.rating = 5
 
 //  Print the updated object and run it to test
 
-
+console.log(favoriteMovie.rating)
 // Task 11: Add a new key to the 'favoriteMovie' object called 'quote' and assign it a string value.  
 // You add a new key the same way that you change a key's value
 
-
+favoriteMovie.quote = "choose me, pick me, love me"
 
 //  Print the updated object and run it to test
 
-
+console.log(favoriteMovie)
 // Task 12: Print a sentence using all the keys inside the object
 // If you are curious, look up template literals for another way to print!
 
-
-// Run your code and make sure everything works before submitting!
+console.log('Hello my name is'+ name + 'I am' + age + "favorite movie is " + favoriteMovie.name + "the genre is " + favoriteMovie.genre + "I rate the movie " + favoriteMovie.rating + "A favorite quote from the movie is "+ favoriteMovie.quote)
+// Run your code and make sure everything works before submittin
